1ae5648ec135a194f6c03f5bfad346c09904e1b7
deepakkdev <deepakkumar19739@gmail.com>
2025-12-13 23:47:48 +0530

    incomplete 3606 coupon code validator

--- 0027_Remove_Element/abc.cpp
+++ 0027_Remove_Element/abc.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int thefunc(){
     // vector<int> nums1 = {1,2,3,0,0,0}, nums2 = {2,5,6};
     vector<int> nums = {3,2,2,3};
-    int val = 3, s =nums.size();
+    int val = 3;
+    int s =nums.size();
     for(int i=0; i<s; i++){
         if(nums[i]==val){
             nums.erase(nums.begin()+ i);
---
+++ 3606_Coupon_Code_Validator/abc.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cctype>
+using namespace std;
+
+vector<string> thefunc(){
+    //vector<string>& code, vector<string>& businessLine, vector<bool>& isActive 
+    // vector<int> nums1 = {1,2,3,0,0,0}, nums2 = {2,5,6};
+    vector<string> code = {"SAVE20","","PHARMA5","SAVE@20"}, businessLine = {"restaurant","grocery","pharmacy","restaurant"};
+    vector<bool> isActive = {true,true,true,true};
+    vector<string> cat = {"electronics", "grocery", "pharmacy", "restaurant"};
+    
+    vector<string> o;
+    vector<bool> bl ;
+    vector<bool> co ;
+    int s = code.size();
+
+    for(int i=0; i<s; i++){
+        bool t=false;
+        for(int j=0; j<s; j++){
+            if(businessLine[i] == cat[j])
+                t=true;
+                break;
+        }
+        bl.push_back(t);
+
+        for (unsigned char c : code[i]){ 
+            if (isalnum(c) && c == '_'){}
+            else {
+                co.push_back(false);
+                break;
+            }
+            co.push_back(true);
+        }
+    }
+    for (const auto& num : bl) {
+    cout << num << " ";
+    }
+    cout << endl;
+    for (const auto& num : co) {
+    cout << num << " ";
+    }
+
+    // for(int i=0; i<s; i++){
+    //     if(isActive[i]){
+    //         if(bl[i]){
+    //             if(){
+
+    //             }
+    //         }
+        
+    //     }
+    // }
+
+
+    cout << endl;
+    return o;
+}
+int main(){
+    cout<<"The Program is working.."<<endl;
+    // cout<<
+    thefunc();
+    return 0;
+}
